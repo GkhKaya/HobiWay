1252f4a3af596bc63081fca99f229e3f50615df2
GkhKaya <gkhkaya00@gmail.com>
2024-10-04 17:10:20 +0300

    Initial Commit

---
+++ HobiWay/HobiWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobiWayApp.swift
+//  HobiWay
+//
+//  Created by Gokhan Kaya on 4.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HobiWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
