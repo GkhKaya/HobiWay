c0a6a630a34564cfcfa1643f13a86832742dbb0f
GkhKaya <gkhkaya00@gmail.com>
2025-01-28 18:06:56 +0300

    Added dark mode to hobby detail view

--- HobiWay/Utils/Widget/HobbyDetailInfoCard.swift
+++ HobiWay/Utils/Widget/HobbyDetailInfoCard.swift
@@ -21,18 +21,19 @@ struct InfoCard: View {
                 
             
             Text(title)
-                .font(.caption)
-                .foregroundColor(.gray)
+                .foregroundStyle(.gray)
+                .modifier(Px12Regular())
             
-            Text(value)
-                .modifier(Px16Regular())
+            Text((value.prefix(1).uppercased() + value.dropFirst()))
+                .foregroundStyle(.winterHaven)
+                .modifier(Px14Regular())
                 .multilineTextAlignment(.center)
-        }
+                        }
         .frame(maxWidth: .infinity)
         .padding()
         .background(
             RoundedRectangle(cornerRadius: ProjectRadius.small.rawValue)
-                .fill(Color.white)
+                .fill(Color.libertyBlue)
                 .shadow(color: Color.black.opacity(0.1), radius: 5)
         )
     }
@@ -45,59 +46,65 @@ struct PhaseCard: View {
     var body: some View {
         VStack(alignment: .leading, spacing: ProjectPaddings.medium.rawValue) {
             HStack {
-                Text(LocalKeys.HobbyDetailView.phease.rawValue)
+                Text(LocalKeys.HobbyDetailView.phease.rawValue.locale())
                     .foregroundColor(.safetyOrange)
                     .modifier(Px18Bold())
                 Text("\(phaseNumber)")
                     .foregroundColor(.safetyOrange)
                     .modifier(Px18Bold())
-                 
                 
                 Spacer()
                 
                 if let duration = phase.duration {
                     Text(duration)
-                        
                         .foregroundColor(.gray)
                         .modifier(Px12Light())
                 }
             }
             
-            if let description = phase.description {
-                Text(description)
-                    .foregroundColor(.gray)
-                    .modifier(Px16Regular())
-                   
-            }
-            
-            if let goals = phase.goals {
-                VStack(alignment: .leading, spacing: ProjectPaddings.small.rawValue) {
-                    Text(LocalKeys.HobbyDetailView.goals.rawValue.locale())
-                        .font(.headline)
+            // ScrollView ile sadece açıklamanın kaymasını sağlıyoruz
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: .leading, spacing: ProjectPaddings.medium.rawValue) {
+                    if let description = phase.description {
+                        Text(description)
+                            .foregroundColor(.gray)
+                            .modifier(Px14Regular())
+                    }
                     
-                    ForEach(goals, id: \.self) { goal in
-                        HStack {
-                            Image(systemName: "circle.fill")
-                                .foregroundColor(.safetyOrange)
-                                .modifier(Px6Light())
-                            Text(goal)
-                                .modifier(Px14Regular())
+                    if let goals = phase.goals {
+                        VStack(alignment: .leading, spacing: ProjectPaddings.small.rawValue) {
+                            Text(LocalKeys.HobbyDetailView.goals.rawValue.locale())
+                                .foregroundStyle(.winterHaven)
+                                .modifier(Px12Bold())
+                            
+                            ForEach(goals, id: \.self) { goal in
+                                HStack {
+                                    Image(systemName: "circle.fill")
+                                        .foregroundColor(.safetyOrange)
+                                        .modifier(Px6Light())
+                                    Text(goal)
+                                        .foregroundStyle(.winterHaven)
+                                        .modifier(Px14Regular())
+                                }
+                            }
                         }
                     }
-                }
-            }
-            
-            if let resources = phase.resources {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(LocalKeys.HobbyDetailView.resources.rawValue.locale())
-                        .font(.headline)
                     
-                    ForEach(resources, id: \.self) { resource in
-                        HStack {
-                            Image(systemName: "link")
-                                .foregroundColor(.safetyOrange)
-                            Text(resource)
-                                .modifier(Px14Regular())
+                    if let resources = phase.resources {
+                        VStack(alignment: .leading, spacing: ProjectPaddings.small.rawValue) {
+                            Text(LocalKeys.HobbyDetailView.resources.rawValue.locale())
+                                .foregroundStyle(.winterHaven)
+                                .modifier(Px12Bold())
+                            
+                            ForEach(resources, id: \.self) { resource in
+                                HStack {
+                                    Image(systemName: "link")
+                                        .foregroundColor(.safetyOrange)
+                                    Text(resource)
+                                        .foregroundStyle(.winterHaven)
+                                        .modifier(Px14Regular())
+                                }
+                            }
                         }
                     }
                 }
@@ -106,10 +113,9 @@ struct PhaseCard: View {
         .padding()
         .background(
             RoundedRectangle(cornerRadius: ProjectRadius.normal.rawValue)
-                .fill(Color.white)
+                .fill(.libertyBlue)
                 .shadow(color: Color.black.opacity(0.1), radius: ProjectRadius.extraSmall.rawValue)
         )
         .padding(.horizontal)
     }
 }
-
